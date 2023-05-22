a9b89b136f3d838012bda28f2dea8dbbcd338a8f
vahnmarty <vahnmarty@gmail.com>
2023-05-22 16:34:55 +0800

    init study

--- app/Http/Livewire/Notes/StudyMode.php
+++ app/Http/Livewire/Notes/StudyMode.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Livewire\Notes;
 
+use App\Models\Note;
 use Livewire\Component;
 
 class StudyMode extends Component
 {
+    public Note $note;
+
+    public $ready = false;
+
     public function render()
     {
-        return view('livewire.notes.study-mode');
+        return view('livewire.notes.study-mode')->layout('layouts.mode');
+    }
+
+    public function mount($uuid)
+    {
+        $this->note = Note::whereUuid($uuid)->firstOrFail();
     }
 }
--- resources/views/livewire/notes/study-mode.blade.php
+++ resources/views/livewire/notes/study-mode.blade.php
@@ -1,3 +1,16 @@
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    <div class="flex justify-center py-1 bg-indigo-500">
+        <p class="text-xs text-white">You are in Study Mode.</p>
+        <a href="{{ route('notes.show', $note->uuid) }}" class="px-3 text-xs text-white underline">
+            <span>Exit</span>
+        </a>
+    </div>
+
+    <div class="py-12">
+        <div class="mx-auto lg:max-w-7xl lg:px-8">
+            <h1 class="text-3xl font-bold">{{ $note->title }}</h1>
+
+            <div class="mt-8 leading-8">{!! $note->contents !!}</div>
+        </div>
+    </div>
 </div>
